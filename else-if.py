e2beb3ac3da761190f4c968d0ac2055b7b85aa58
Leyla Hunn <47248244+leylahunn@users.noreply.github.com>
2019-04-02 13:58:38 +0100

    Create else-if.py

---
+++ else-if.py
@@ -0,0 +1,11 @@
+x = 4
+y = 8
+if x == y:
+    print('x and y are equal')
+    if y !=0:
+        print('therefore x/y is', x/y)
+    
+elif x > y:
+    print('x is bigger than y')
+else:
+    print('y is bigger')
