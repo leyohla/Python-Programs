4852aa3b5a22d30c9eaa8843856b47776a6b0fbf
Leyla Hunn <47248244+leylahunn@users.noreply.github.com>
2019-04-02 16:50:07 +0100

    Create loops.py

---
+++ loops.py
@@ -0,0 +1,22 @@
+#infinite loops
+
+while 1:
+    number = input('Enter number: ')
+    if number == '4': break
+output:
+Enter number: 
+Enter number:
+Enter number:
+
+#Asks for user input before each iteration, and loops once user inputs a number.
+    
+#dictionaries
+
+ages = {'me': 22, 'someone': 32, 'someoneelse': 65}
+for item in ages:
+    print item
+    
+output: 
+me
+someone
+someoneelse
